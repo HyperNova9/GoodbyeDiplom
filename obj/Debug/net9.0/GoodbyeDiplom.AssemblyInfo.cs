5ed02727567cde262a6dab2c04f283b0fa8b6da4
HyperNova9 <artem.lyubchenko41@gmail.com>
2025-04-18 15:33:33 +0700

    Мелкие исправления #2

--- obj/Debug/net9.0/GoodbyeDiplom.AssemblyInfo.cs
+++ obj/Debug/net9.0/GoodbyeDiplom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoodbyeDiplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a0658a5cee6c560359043b2b8227331a7b43b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0259ba68a18781d9744465922592101b839c0df")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoodbyeDiplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoodbyeDiplom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
